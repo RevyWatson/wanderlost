15f1e4620d0bec799b3f0b5624ecc13a172a0d1f
Dracon75 <nanorth75@gmail.com>
2021-03-06 14:10:14 -0500

    added fetch request for openstreet api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,32 @@
 console.log("Too much time on our hands");
 
-const userMap = L.map('cityMapId').setView([51.505, -0.09], 13);
-
+//copywrite attribution reqs
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-
+//variable data needed to display map
+const userMap = L.map('cityMapId').setView([51.505, -0.09], 13);
+//conecting url for image tiles
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const tiles = L.tileLayer(tileUrl, [attribution]);
+//display chosen photo tiles to map
 tiles.addTo(userMap);
+//openstree api data retrieval function
+function getApi(userCity) {
+    console.log(userCity);
+    let destinationEl = document.getElementById("destination");
+    let finalDestination = destinationEl.value;
+    console.log(finalDestination);
 
-const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+    fetch(titleUrl, {
+      method: "GET",
+      withCredentials: true
+    })
+      .then(response => response.json())
+      .then(function (data) {
+        console.log(data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+getApi();
