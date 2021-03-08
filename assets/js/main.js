af14f6d1d0891e38978e00d83f2ccf778dd30e96
Dracon75 <nanorth75@gmail.com>
2021-03-08 14:18:16 -0500

    added weather widget stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,84 @@
 console.log("Too much time on our hands");
+//map stuff
+//variables for map coordinates
+const lat = 51.505
+const long = -0.09
+const zoom = 13
 
-const userMap = L.map('cityMapId').setView([51.505, -0.09], 13);
+//variable for map
+const userMap = L.map('cityMapId').setView([lat, long], zoom);
 
+//attribution variable
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
+//varriables for map photo tiles
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const tiles = L.tileLayer(tileUrl, [attribution]);
+
+//function adding tiles to map
 tiles.addTo(userMap);
 
-const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+//weather stuff
+
+
+const cityInput = document.querySelector("#cityInput");
+const form = document.getElementById('search-form');
+
+let cityName = localStorage.getItem("cityName");
+let cities = [];
+
+//listen for when submit button is clicked
+form.addEventListener('submit', logSubmit);
+
+function logSubmit(event) {
+  log.textContent = `Form Submitted!`;
+  event.preventDefault();
+  let userCity = cityInput.value;
+  localStorage.setItem("cityName", userCity);
+  getApi(userCity);
+}
+
+
+//function that retrieves data from third-party api for a specific location
+function getApi(userCity) {
+  console.log(userCity);
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&units=imperial&appid=23862a5f23b4f0870e633a28562d8b34";
+  fetch(url, {
+    method: "GET",
+    withCredentials: true
+  })
+    .then(response => response.json())
+    .then(function (data) {
+      console.log(data);
+  
+      const nameVal = data['name'];
+      const tempVal = data['main']['temp'];
+      const humVal = data['main']['humidity'];
+      const windVal = data['wind']['speed'];
+      const uvVal = data['coord']['lat']
+      const uviVal = data['coord']['lon']
+    
+      document.getElementById("name").innerHTML = "name: " + nameVal;
+      console.log("nameVal");
+      document.getElementById("temp").innerHTML = "Tempature: " + tempVal;
+      document.getElementById("humidity").innerHTML = "Humidity: " + humVal;
+      document.getElementById("wind-speed").innerHTML = "Wind Speed: " + windVal;
+      document.getElementById("uv-index").innerHTML = "UV Index: " + uvVal + " " + uviVal;
+
+      console.log(nameVal, tempVal, humVal, windVal, uvVal, uviVal);
+      if (windVal > 5) {
+        document.getElementById('main').className = "fas fa-wind";
+        document.getElementById('main').innerHTML = "feels windy";
+      }
+      else {
+        document.getElementById('main').className = "fas fa-sun";
+        document.getElementById('main').innerHTML = "feels nice";
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+}
